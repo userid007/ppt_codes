a9d21a1cc1e98a6fc51f25907e8a7f4cde987f1f
userid007 <shindeakshay7499@gmail.com>
2023-06-03 23:16:39 +0530

    A4-Q4 done, A2-Q1 fix

--- Assignment_4_2D_Arrays/Q4_Max_Min_Sum.cpp
+++ Assignment_4_2D_Arrays/Q4_Max_Min_Sum.cpp
@@ -1,4 +1,4 @@
-// **Question 1**
+// **Question 4**
 // Given an integer array nums of 2n integers, group these integers into n pairs (a1, b1), (a2, b2),..., (an, bn)
 // such that the sum of min(ai, bi) for all i is maximized. Return the maximized sum.
 
@@ -18,7 +18,6 @@ using namespace std;
 
 class Solution
 {
-  
 public:
   int maxMinSum(vector<int> &nums)
   {
