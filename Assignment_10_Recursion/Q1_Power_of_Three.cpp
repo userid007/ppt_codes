614c58dc78655681e14aa55e73e8385990cb7be9
userid007 <shindeakshay7499@gmail.com>
2023-06-11 21:01:37 +0530

    A10-Q1 done

---
+++ Assignment_10_Recursion/Q1_Power_of_Three.cpp
@@ -0,0 +1,45 @@
+// Given an integer n, return true if it is a power of three. Otherwise, return false.
+
+// An integer n is a power of three, if there exists an integer x such that n == 3x.
+
+// Example 1:
+
+// Input: n = 27
+// Output: true
+// Explanation: 27 = 33
+// Example 2:
+
+// Input: n = 0
+// Output: false
+// Explanation: There is no x where 3x = 0.
+// Example 3:
+
+// Input: n = -1
+// Output: false
+// Explanation: There is no x where 3x = (-1).
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+  bool isPowerOfThree(int n)
+  {
+    if (n <= 0)
+      return false;
+    if (n == 1)
+      return true;
+    return n % 3 == 0 && isPowerOfThree(n / 3);
+  }
+};
+
+int main()
+{
+  int n;
+  cin >> n;
+  Solution S;
+  cout << S.isPowerOfThree(n) << endl;
+  return 0;
+}
