465283b8b9892c639745c89518a0f9f29d9939ef
userid007 <shindeakshay7499@gmail.com>
2023-06-14 00:33:01 +0530

    A11-Q8 done, A11-Q5 fix

--- Assignment_11_Binary_Search/Q8_Intersection_of_Two_Arrays_II.cpp
+++ Assignment_11_Binary_Search/Q8_Intersection_of_Two_Arrays_II.cpp
@@ -1,26 +1,24 @@
-// Question 5
+// Question 8
 // Given two integer arrays nums1 and nums2, return an array of their intersection.
-// Each element in the result must be unique and you may return the result in any order.
+// Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
 
 // Example 1:
-
 // Input: nums1 = [1,2,2,1], nums2 = [2,2]
-// Output: [2]
-// Example 2:
+// Output: [2,2]
 
+// Example 2:
 // Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
-// Output: [9,4]
-// Explanation: [4,9] is also accepted.
+// Output: [4,9]
+// Explanation: [9,4] is also accepted.
 
 #include <iostream>
 #include <vector>
-#include <unordered_set>
 using namespace std;
 
 class Solution
 {
 public:
-  vector<int> intersection(vector<int> &nums1, vector<int> &nums2)
+  vector<int> intersect(vector<int> &nums1, vector<int> &nums2)
   {
     vector<int> ans;
     sort(nums1.begin(), nums1.end());
@@ -31,9 +29,8 @@ public:
 
     for (int num2 : nums2)
     {
-      if (!ans.size() || ans.back() != num2)
-        if (binarySearch(nums1, num2))
-          ans.push_back(num2);
+      if (binarySearch(nums1, num2))
+        ans.push_back(num2);
     }
     return ans;
   }
@@ -46,6 +43,7 @@ public:
       mid = left + (right - left) / 2;
       if (nums[mid] == target)
       {
+        nums.erase(nums.begin() + mid);
         return true;
       }
       else if (nums[mid] < target)
@@ -76,10 +74,5 @@ int main()
     cin >> nums2[i];
 
   Solution S;
-  vector<int> ans = S.intersection(nums1, nums2);
-  cout << "[";
-  for (int num : ans)
-    cout << num << ",";
-  cout << "\b]\n";
   return 0;
 }
